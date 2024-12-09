2f30d58a10d53d28ddbfdeeffbb59c74d8ecdb6b
izumit <75725726+MitsuoKoizumi@users.noreply.github.com>
2024-12-09 20:18:41 +0900

    Revert "人口動態アプリの作成"

--- data/data.ts
+++
@@ -1,7 +0,0 @@
-const data = [
-  { name: "A", value: 400 },
-  { name: "B", value: 200 },
-  { name: "C", value: 300 },
-  { name: "D", value: 350 },
-  { name: "E", value: 300 },
-];
--- src/app/types.ts
+++
@@ -1,9 +0,0 @@
-export type PopulationData = {
-  label: string;
-  data: { year: number; value: number; rate: number }[];
-};
-
-export type Prefecture = {
-  prefCode: number;
-  prefName: string;
-};
